ac756879b0c73abced411d6c750aac57a5f26f87
chandubabubangaram <41461786+chandubabubangaram@users.noreply.github.com>
2018-07-22 12:00:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,45 @@
-function Loadjson(file,callback){
-  var x = new XMLHttpRequest();
-  x.overrideMimeType("application/json");
-  x.open("GET",file,true);
-  x.onreadystatechange=function(){
-    if(x.readyState === 4 && x.status == "200"){
-      callback(x.responseText);
-    }
-  };
-  x.send(null);
-}
+// function Loadjson(file,callback){
+//   var x = new XMLHttpRequest();
+//   x.overrideMimeType("application/json");
+//   x.open("GET",file,true);
+//   x.onreadystatechange=function(){
+//     if(x.readyState === 4 && x.status == "200"){
+//       callback(x.responseText);
+//     }
+//   };
+//   x.send(null);
+// }
+//
+// Loadjson("data.json",function(text){
+//   var data = JSON.parse(text);
+//   console.log(data);
+//   basics(data.details);
+//   career(data.career);
+//   education(data.education);
+//   keyskills(data.keyskills);
+//   achievements(data.achievements)
+// })
 
-Loadjson("data.json",function(text){
-  var data = JSON.parse(text);
+function loadjson(file){
+  return new Promise((resolve,reject)=>{
+    return fetch(file).then(response=>{
+      if(response.ok){
+        resolve(response.json());
+      }
+      else{
+        reject(new Error("error"));
+      }
+    })
+  })
+}
+var file =loadjson("data.json");
+file.then(data=>{
   console.log(data);
   basics(data.details);
   career(data.career);
   education(data.education);
   keyskills(data.keyskills);
-  achievements(data.achievements)
+ achievements(data.achievements)
 })
 
 var child1 = document.querySelector(".child1");
